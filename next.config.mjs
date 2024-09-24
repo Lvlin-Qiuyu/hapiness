511bdd952d7f3eaafc623ab0822208c603c4eb50
LvLin <lvaluesilence@gmail.com>
2024-09-24 23:26:02 +0800

    增加基础路径

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-    // basePath: "/happiness-pages",
+    basePath: "/happiness",
     images: {
         unoptimized: true,
     },
